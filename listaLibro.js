8bb0e6edc6e5654a2aadefccae2259307417e7ff
Death Gun24 <miguel_guzman6@usmp.pe>
2024-04-17 03:38:13 -0500

    final commit

--- listaLibro.js
+++ listaLibro.js
@@ -7,11 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
         libros.forEach(libro => {
             const libroHTML = `
                 <div style="background-color:#F5F5F5;padding: 5px;" class="col-3">
-                    <img style="width: 300px;height: 390px;" src="${libro.portada}" alt="">
+                    <img class="zoom" style="width: 300px;height: 390px;" src="${libro.portada}" alt="">
                     <ul>
                         <li>Nombre del libro: ${libro.titulo}</li>
-                        <li>Nombre del autor: ${libro.autor}</li>
+                        <li>Nombre del dueño: ${libro.autor}</li>
                         <li>Número de contacto: ${libro.celular}</li>
+                        <li>Estado del libro: <span class="rating">&#9733;&#9733;&#9733;&#9733;&#9733;</span></li>
                     </ul>
                 </div>
             `;
